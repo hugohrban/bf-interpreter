3daa8771825565576fc2c263a282ad9ff4decdb5
Hugo Hrbáň <hugohrban2@gmail.com>
2023-05-30 12:28:21 +0200

    prog docs, cleanup and cmd args

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,174 +1,182 @@
 import Control.Monad.Except
+import Data.Functor ((<&>))
+import Data.Word (Word8)
+import System.Environment (getArgs)
 import Prelude hiding (Left, Right)
-import Data.Functor
-import Data.Int (Int8, Int16)
-import System.Environment
 
+-- This program is an interpreter for the Brainfuck programming language implemented in Haskell.
+-- Language reference: http://brainfuck.org/brainfuck.html
 
--- tape is the BF memory (size is 30000, starting at index 0), 
--- left side is reversed to make it easier (and faster) to work with
-data Tape = Tape [Int8] Int8 [Int8] deriving (Show)
-
-data Instruction =
-    Inc |
-    Dec |
-    Left |
-    Right |
-    Print |
-    Read |
-    Loop [Instruction]
-    deriving (Show)
-
--- the BF program is a list of instructions
+-- the Brainfuck program is a list of instructions
 type Program = [Instruction]
 
+-- Brainfuck instructions:
+--      '+' -> Increment value at pointer
+--      '-' -> Decrement value at pointer
+--      '<' -> Move pointer left
+--      '>' -> Move pointer right
+--      '.' -> Print character at pointer to stdout
+--      ',' -> Read one character from stdin and store it at the pointer
+--      '[' -> Start of loop - only enter the loop if value at pointer is non-zero. The loop contains a list of instructions
+--      ']' -> End of loop - if value at pointer is non-zero repeat the loop
+data Instruction
+  = Inc
+  | Dec
+  | Left
+  | Right
+  | Print
+  | Read
+  | Loop [Instruction]
+  deriving (Show, Eq)
+
+-- tape is the BF memory (size is 30000, starting at index 0),
+-- left side is reversed to make it easier (and faster) to work with
+-- if user moves pointer outside the tape, error is thrown
+-- the elements are of type Word8 which is a 8-bit unsigned integer
+data Tape = Tape [Word8] Word8 [Word8] deriving (Show)
+
+-------------------- Instructions --------------------
+
 inc :: Tape -> Tape
-inc (Tape l p r) = Tape l (p+1) r
+inc (Tape l p r) = Tape l (p + 1) r
 
 dec :: Tape -> Tape
-dec (Tape ls p rs) = Tape ls (p-1) rs
+dec (Tape ls p rs) = Tape ls (p - 1) rs
 
 left :: Tape -> Tape
-left (Tape (l:ls) p rs) = Tape ls l (p:rs)
+left (Tape (l : ls) p rs) = Tape ls l (p : rs)
 left (Tape [] _ _) = error "left end of tape"
 
 right :: Tape -> Tape
-right (Tape ls p (r:rs)) = Tape (p:ls) r rs
+right (Tape ls p (r : rs)) = Tape (p : ls) r rs
 right (Tape _ _ []) = error "right end of tape"
 
-get :: Tape -> Int8
+get :: Tape -> Word8
 get (Tape _ p _) = p
 
 printChar :: Tape -> IO Tape
 printChar t = do
-    putChar $ toEnum $ fromEnum $ get t
-    return t
+  putChar $ toEnum $ fromEnum $ get t
+  return t
+
+set :: Tape -> Word8 -> Tape
+set (Tape ls _ rs) p = Tape ls p rs
 
 readChar :: Tape -> IO Tape
 readChar t = do
-    ch <- getChar
-    -- if end of file
-    --if ch == '\n' then return t else do
-    let i = toEnum $ fromEnum ch
-    return $ set t i
-
-set :: Tape -> Int8 -> Tape
-set (Tape ls _ rs) p = Tape ls p rs
-
-
---- print a portion of tape around the pointer
-debugPrint :: IO Tape -> Int -> IO ()
-debugPrint t i = do
-    (Tape l p r) <- t
-    putStrLn $ show (take i $ reverse l) ++ " " ++ show p ++ " " ++ show (take i r)
-
-
--- get only the valid BF characters . , < > + - [ ]
-removeUnwantedChars :: String -> String
-removeUnwantedChars = filter (`elem` ".,+-<>[]")
-
-
--- check if the brackets are matched
-checkMatchingBrackets :: String -> Bool
-checkMatchingBrackets = check 0
-    where
-        check n [] = n == 0
-        check n (x:xs)
-            | n < 0 = False
-            | x == '[' = check (n+1) xs
-            | x == ']' = check (n-1) xs
-            | otherwise = check n xs
-
-
--- parse the first instruction from source code, (if its a '[' it will be a longer list), and also return the restof source code
-parseInstruction :: String -> ([Instruction], String)
-parseInstruction [] = ([],[])
-parseInstruction (i:is)
-    | i == '.' = ([Print], is)
-    | i == ',' = ([Read], is)
-    | i == '+' = ([Inc], is)
-    | i == '-' = ([Dec], is)
-    | i == '<' = ([Left], is)
-    | i == '>' = ([Right], is)
-    | i == '[' = ([Loop (parseSourceCode l)], r)
-        where (l,r) = getBracketedCode (i:is)
-
-
--- get all chars between this and the matching bracket and the rest, assuming first char is bracket, output will be ([.*], .*)
-getBracketedCode :: String -> (String, String)
-getBracketedCode [] = ([],[])
-getBracketedCode (x:xs) = check 1 [x] xs
-    where
-        check 0 left right = (init $ tail $ reverse left, right)
-        check n ls (r:rs)
-            | r == '[' = check (n+1) (r:ls) rs
-            | r == ']' = check (n-1) (r:ls) rs
-            | otherwise = check n (r:ls) rs
-
--- assuming source code is verified and cleaned up, parse it into a list of instructions
-parseSourceCode :: String -> Program
-parseSourceCode code = parse code []
-    where
-        parse [] prog = prog
-        parse xs prog = prog ++ parsed ++ parseSourceCode rest
-            where (parsed, rest) = parseInstruction xs
-
-getProgram :: String -> Program
-getProgram code
-    | not $ checkMatchingBrackets code = error "unmatched brackets"
-    | otherwise = parseSourceCode $ removeUnwantedChars code
-
-makeTape :: Int -> Tape
-makeTape n = Tape [] 0 (replicate (n-1) 0)
-
--- normal tape size
-normtape :: Tape
-normtape = makeTape 30000
+  ch <- getChar
+  let i = toEnum $ fromEnum ch
+  return $ set t i
 
 executeLoop :: Instruction -> Tape -> IO Tape
 executeLoop (Loop is) t = do
-    if get t == 0                   -- if the pointer is 0, don't execute the loop
-        then return t
-    else do                    
-        t' <- execute is (return t)     -- execute the loop body
-        executeLoop (Loop is) t'        -- execute the loop again recursively
+  if get t == 0 -- if the pointer is 0, don't execute the loop
+    then return t
+    else do
+      t' <- execute is (return t) -- execute the loop body
+      executeLoop (Loop is) t' -- execute the whole loop again recursively
 
-    -- t' <- execute is (return t)
-    -- if get t' == 0 then return t' else executeLoop (Loop is) t'
+-------------------- Debug --------------------
 
+--- print a portion of tape around the pointer for debugging purposes
+debugPrint :: IO Tape -> Int -> IO ()
+debugPrint t i = do
+  (Tape l p r) <- t
+  putStrLn $ show (take i $ reverse l) ++ " " ++ show p ++ " " ++ show (take i r)
 
--- TODO understand this, fix Read
-executeInstruction :: Instruction -> IO Tape -> IO Tape
-executeInstruction Inc t = t >>= return . inc
-executeInstruction Dec t = t >>= return . dec
-executeInstruction Left t = t >>= return . left
-executeInstruction Right t = t >>= return . right
-executeInstruction Print t = t >>= printChar
-executeInstruction Read t = t >>= readChar
-executeInstruction (Loop is) t = t >>= executeLoop (Loop is)
+-------------------- Parsing --------------------
 
--- \t' -> if get t' == 0 then return t' else execute is (return t') >>= \t'' -> executeInstruction (Loop is) (return t'')
+-- check if the brackets are matched correctly
+checkMatchingBrackets :: String -> Bool
+checkMatchingBrackets code = check 0 $ filter (`elem` "[]") code
+  where
+    check n [] = n == 0 -- n is the number uf currently open brackets
+    check n (x : xs)
+      | n < 0 = False
+      | x == '[' = check (n + 1) xs
+      | x == ']' = check (n - 1) xs
+      | otherwise = check n xs -- this shouldn't happen since we filter
 
--- TODO
+-- parse the first instruction from source code, (if its a '[' parse the inside recursively using parseSourceCode).
+-- returns ([parsedInstruction], restOfCode). note: we need to return rest of code because the loop can have arbitrary length
+parseInstruction :: String -> ([Instruction], String)
+parseInstruction [] = ([], [])
+parseInstruction (i : is)
+  | i == '.' = ([Print], is)
+  | i == ',' = ([Read], is)
+  | i == '+' = ([Inc], is)
+  | i == '-' = ([Dec], is)
+  | i == '<' = ([Left], is)
+  | i == '>' = ([Right], is)
+  | i == '[' = ([Loop (parseSourceCode l)], r)
+  where
+    (l, r) = getBracketedCode (i : is)
+
+-- get all chars between this and the matching bracket and the rest, assuming first char is bracket, output will be (codeInBrackets, restOfCode)
+getBracketedCode :: String -> (String, String)
+getBracketedCode [] = ([], [])
+getBracketedCode (x : xs) = check 1 [x] xs
+  where
+    check 0 left right = (init $ tail $ reverse left, right)
+    check n ls (r : rs)
+      | r == '[' = check (n + 1) (r : ls) rs
+      | r == ']' = check (n - 1) (r : ls) rs
+      | otherwise = check n (r : ls) rs
+
+-- parse the source code string into a BF program. throws error if brackets are unmatched
+parseSourceCode :: String -> Program
+parseSourceCode code
+  | not $ checkMatchingBrackets code = error "unmatched brackets"
+  | otherwise = parse (filter (`elem` "+-<>[],.") code) []
+  where
+    parse :: String -> Program -> Program
+    parse [] prog = prog
+    parse xs prog = prog ++ parsed ++ parseSourceCode rest
+      where
+        (parsed, rest) = parseInstruction xs
+
+-------------------- Execution --------------------
+
+-- make a tape of size n
+makeTape :: Int -> Tape
+makeTape n = Tape [] 0 (replicate (n - 1) 0)
+
+-- normal tape size is 30000 as per the BF spec
+normTape :: Tape
+normTape = makeTape 30000
+
+-- execute a single instruction on a tape and return the new tape
+executeInstruction :: IO Tape -> Instruction -> IO Tape
+executeInstruction t ins
+  | ins == Inc = t <&> inc
+  | ins == Dec = t <&> dec
+  | ins == Left = t <&> left
+  | ins == Right = t <&> right
+  | ins == Print = t >>= printChar
+  | ins == Read = t >>= readChar
+  | otherwise = t >>= executeLoop ins
+
+-- execute a program on a tape and return the final tape
 execute :: Program -> IO Tape -> IO Tape
--- execute [] t = t
--- execute (i:is) t = execute is (executeInstruction i t)
-execute is t = foldl (flip executeInstruction) t is
+execute is t = foldl executeInstruction t is
 
+-- run a BF program from a string of source code
 run :: String -> IO ()
 run code = do
-    execute (getProgram code) (return normtape)
-    return ()
+  execute (parseSourceCode code) (return normTape)
+  return ()
 
+-- run a BF program from a file
 runFile :: String -> IO ()
 runFile filename = do
-    code <- readFile filename
-    execute (getProgram code) (return normtape)
-    return ()
+  code <- readFile filename
+  execute (parseSourceCode code) (return normTape)
+  return ()
 
 main :: IO ()
 main = do
-    args <- getArgs
-    code <- readFile $ head args
-    execute (getProgram code) (return normtape)
-    return ()
+  args <- getArgs
+  code <- readFile $ head args
+  let tape = if length args > 1 then return $ makeTape $ read $ args !! 1 else return normTape
+  execute (parseSourceCode code) tape
+  return ()
